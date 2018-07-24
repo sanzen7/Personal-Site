6c34e7939baf9394074957aafb6dab800670fa08
Steven Carranza | Decibel <scarranza@decibelinsight.com>
2018-07-24 09:14:39 -0600

    changing h1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Welcome to Reactstrap</h1>
+        <h1>Steven J. Carranza</h1>
         <Navigator />
         <img className="bannerPhoto" src={mainBanner} alt="space main"/>
       </div>
